609b651fdda35d89ffdd9a9e9c763d91d889caec
Alexander Medvedev <mega.medved87@gmail.com>
2023-11-28 11:27:51 -0600

    #1 ready for building components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
-}
+const App = () => {
+  return(
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        Hero
+      </section>
+      <section className="padding">
+        Popular
+      </section>
+      <section className="padding">
+       SuperQuality
+      </section>
+      <section className="padding-x py-10">
+        Services
+      </section>
+      <section className="padding">
+        SpecialOffer
+      </section>
+      <section className="padding bg-pale-blue padding">
+       Rewiew
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        Subscribe
+      </section>
+      <section className="padding-x padding-t pb-8">
+        Footer
+      </section>
+    </main>
+  );
+};
 
 export default App
